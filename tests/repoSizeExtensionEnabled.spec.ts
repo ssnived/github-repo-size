8e30451ab7263ca9ce7eb5503d831886aec4f722
ssnived <ssniv@hotmail.com>
2024-01-17 19:44:09 +1100

    fixed page loading race condition

--- tests/repoSizeExtensionEnabled.spec.ts
+++ tests/repoSizeExtensionEnabled.spec.ts
@@ -2,14 +2,14 @@ import { test, expect } from "./fixtures/fixtures";
 import { repoSizeColumnPage } from "./page-object/repo-size-column";
 
 test.describe("Extension Enabled: Repo size column tests", () => {
-  test("Extension Enabled: Validate the column exists and is visible with appropriate MEASURE's in the DOM", async ({
+  test.only("Extension Enabled: Validate the column exists and is visible with appropriate MEASURE's in the DOM", async ({
     page,
   }) => {
     const repoSizeColumn = new repoSizeColumnPage(page);
     await page.goto(repoSizeColumn.pageURL);
-    // await page.waitForSelector('[data-testid*="github-repo-size-cell"]');
+    await page.waitForSelector('[data-testid*="github-repo-size-cell"]');
 
-    await repoSizeColumn.validateCellsAreVisible();
+    await expect(await repoSizeColumn.validateCellsAreVisible()).toBe(true);
 
     // const numberOfCells = await page
     //   .locator('[data-testid*="github-repo-size-cell"]')
